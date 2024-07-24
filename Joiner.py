c540b356bcc82dff098d380f33c33fdc14868f4c
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-07-24 08:31:28 -0600

    joined all raw files into one, removed garbage when merging all the pdfs

--- Joiner.py
+++ Joiner.py
@@ -9,6 +9,13 @@ for raw_department in os.listdir('./RAW_FILES'):
     raw_df = raw_df.loc[:, ~raw_df.columns.str.contains('^Unnamed')]
     raw_dfs.append(raw_df)
 df = pd.concat(raw_dfs,ignore_index=True)
+
+#remover los vaciós o la basura prococada por alerts y conteo entre los datos
+regex = r"([0-9]+( )*Establecimientos encontrados|alert\('.*'\))"
+unvalid = (df['CODIGO'].str.contains(regex).astype(bool))
+df = df.loc[~unvalid]
+df.dropna(how="all")
 df.to_csv('DEPARTAMENTOS.csv',index=False)
+
 del raw_dfs
 del df
